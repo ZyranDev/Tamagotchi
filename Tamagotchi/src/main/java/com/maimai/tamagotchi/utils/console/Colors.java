0782576417687ba4c679b05066ea374ec01b587a
JusTinDev1 <72838688+JusTinDev1@users.noreply.github.com>
2021-02-26 21:30:36 -0400

    added Colors class supported by console

---
+++ Tamagotchi/src/main/java/com/maimai/tamagotchi/utils/console/Colors.java
@@ -0,0 +1,19 @@
+package com.maimai.tamagotchi.utils.console;
+
+public class Colors {
+
+    public String black = "\033[30m";
+    public String red = "\033[31m";
+    public String green = "\033[32m";
+    public String yellow = "\033[33m";
+    public String blue = "\033[34m";
+    public String purple = "\033[35m";
+    public String cyan = "\033[36m";
+    public String white = "\033[37m";
+    public String reset = "\u001B[0m";
+
+    public Colors(){
+
+    }
+
+}
