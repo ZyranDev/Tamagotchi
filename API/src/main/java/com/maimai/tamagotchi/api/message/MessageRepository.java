d1fc48626054e7d734ab6da251dc490b8e775525
ImTheNico <68520721+ImTheNico@users.noreply.github.com>
2021-03-12 02:00:29 -0300

    Create MessageRepository.java

---
+++ API/src/main/java/com/maimai/tamagotchi/api/message/MessageRepository.java
@@ -0,0 +1,46 @@
+package com.maimai.tamagotchi.api.message;
+
+import com.maimai.tamagotchi.api.command.CommandListener;
+import com.maimai.tamagotchi.api.command.CommandResponse;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+public class MessageRepository implements CommandListener {
+
+    private final Map<String, String> messages;
+
+    public MessageRepository() {
+        this.messages = new HashMap<>();
+        addMessage("unknown-command", "Unknown command, type \"help\" to see command list.");
+    }
+
+    @Override
+    public void listen(CommandResponse commandResponse) {
+        if (commandResponse instanceof MessageCommandResponse) {
+            System.out.println(messages.get(((MessageCommandResponse) commandResponse).path));
+        }
+    }
+
+    public String getMessage(String path) {
+        return messages.get(path);
+    }
+
+    public void addMessage(String path, String message) {
+        this.messages.put(Objects.requireNonNull(path), Objects.requireNonNull(message));
+    }
+
+    public static CommandResponse commandResponse(String path) {
+        return new MessageCommandResponse(path);
+    }
+
+    private static class MessageCommandResponse implements CommandResponse {
+
+        private final String path;
+
+        public MessageCommandResponse(String path) {
+            this.path = path;
+        }
+    }
+}
