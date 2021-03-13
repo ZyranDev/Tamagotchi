2849c2f868b5537a6ec85596690f781ab14c87d3
ImTheNico <68520721+ImTheNico@users.noreply.github.com>
2021-03-13 17:17:44 -0300

    Updated TamagotchiService to get another services

--- Tamagotchi/src/main/java/com/maimai/tamagotchi/service/TamagotchiService.java
+++ Tamagotchi/src/main/java/com/maimai/tamagotchi/service/TamagotchiService.java
@@ -1,29 +1,27 @@
 package com.maimai.tamagotchi.service;
 
-import com.maimai.tamagotchi.api.command.CommandManager;
-import com.maimai.tamagotchi.api.command.manager.SimpleCommandManager;
 import com.maimai.tamagotchi.api.service.Service;
-import com.maimai.tamagotchi.command.UserModelPartProvider;
 import com.maimai.tamagotchi.manager.GameSessionManager;
 import com.maimai.tamagotchi.api.manager.Manager;
 import com.maimai.tamagotchi.session.GameSession;
 import com.maimai.tamagotchi.api.user.UserModel;
 
 import java.io.File;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
 
 public class TamagotchiService implements Service {
 
-    private final Set<Service> services;
+    private final Map<Class<? extends Service>, Service> services;
 
     private final Manager<String, GameSession> gameSessionManager;
 
     private final UserModel userModel;
 
     public TamagotchiService() {
-        this.services = new HashSet<>();
-        this.services.add(new TerminalService(this));
+        this.services = new HashMap<>();
+        this.services.put(TerminalService.class, new TerminalService(this));
 
         this.gameSessionManager = new GameSessionManager(new File("saved games"));
 
@@ -31,11 +29,16 @@ public class TamagotchiService implements Service {
     }
 
     public void start() {
-        services.forEach(Service::start);
+        services.values().forEach(Service::start);
     }
 
     public void stop() {
-        services.forEach(Service::stop);
+        services.values().forEach(Service::stop);
+    }
+
+    @SuppressWarnings("unchecked")
+    public <T extends Service> T getService(Class<T> tClass) {
+        return (T) services.get(tClass);
     }
 
     public UserModel getUser() {
