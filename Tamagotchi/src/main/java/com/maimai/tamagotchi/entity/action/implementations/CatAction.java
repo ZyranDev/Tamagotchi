52293ef0b23c5453ddbe83951a19d656d1033adf
vCamilx <72714741+vCamilx@users.noreply.github.com>
2021-03-16 10:33:25 -0500

    Basis of actions created
    Base actions have been created, as well as an implementation to perhaps later add to the API.

---
+++ Tamagotchi/src/main/java/com/maimai/tamagotchi/entity/action/implementations/CatAction.java
@@ -0,0 +1,40 @@
+package com.maimai.tamagotchi.entity.action.implementations;
+
+import com.maimai.tamagotchi.entity.action.ActionModel;
+
+public class CatAction implements ActionModel {
+    /* Default actions */
+    @Override
+    public void drinkAction() {
+
+    }
+
+    @Override
+    public void eatAction() {
+
+    }
+
+    @Override
+    public void jumpAction() {
+
+    }
+
+    @Override
+    public void runAction() {
+
+    }
+
+    @Override
+    public void sleepAction() {
+
+    }
+
+    /* Special actions */
+    public void purrAction() {
+
+    }
+
+    public void somersaultAction() {
+
+    }
+}
---
+++ Tamagotchi/src/main/java/com/maimai/tamagotchi/entity/action/implementations/RabbitAction.java
@@ -0,0 +1,40 @@
+package com.maimai.tamagotchi.entity.action.implementations;
+
+import com.maimai.tamagotchi.entity.action.ActionModel;
+
+public class RabbitAction implements ActionModel {
+    /* Default actions */
+    @Override
+    public void drinkAction() {
+
+    }
+
+    @Override
+    public void eatAction() {
+
+    }
+
+    @Override
+    public void jumpAction() {
+
+    }
+
+    @Override
+    public void runAction() {
+
+    }
+
+    @Override
+    public void sleepAction() {
+
+    }
+
+    /* Special actions */
+    public void biteAction() {
+
+    }
+
+    public void kickAction() {
+
+    }
+}
