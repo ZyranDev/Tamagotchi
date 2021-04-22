7a71b3cc103d8ca4ea724fe0d15a90e6b3780e00
vCamilx <72714741+vCamilx@users.noreply.github.com>
2021-04-22 18:45:47 -0500

    Update TamagotchiLauncher.java

--- Tamagotchi/src/main/java/com/maimai/tamagotchi/TamagotchiLauncher.java
+++ Tamagotchi/src/main/java/com/maimai/tamagotchi/TamagotchiLauncher.java
@@ -1,7 +1,7 @@
 package com.maimai.tamagotchi;
 
 import com.maimai.tamagotchi.module.MainModule;
-import com.maimai.tamagotchi.service.implementation.TamagotchiServiceImpl;
+import com.maimai.tamagotchi.service.IService;
 import me.yushust.inject.Injector;
 
 import javax.inject.Inject;
@@ -11,13 +11,12 @@ public class TamagotchiLauncher {
 
     @Inject
     @Named("tamagotchi-service")
-    private static TamagotchiServiceImpl tamagotchiServiceImpl;
-
+    private static IService tamagotchiService;
 
     public static void main(String[] args) {
         Injector injector = Injector.create(new MainModule());
         injector.injectStaticMembers(TamagotchiLauncher.class);
 
-        tamagotchiServiceImpl.start();
+        tamagotchiService.start();
     }
 }
