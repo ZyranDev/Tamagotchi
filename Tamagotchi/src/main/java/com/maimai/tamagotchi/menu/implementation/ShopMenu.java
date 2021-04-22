3bc62e47868c62ac25ff4bf2c756685d8b9aeee1
vCamilx <72714741+vCamilx@users.noreply.github.com>
2021-04-22 18:52:37 -0500

    Debug MoneyManager

--- Tamagotchi/src/main/java/com/maimai/tamagotchi/menu/implementation/ShopMenu.java
+++ Tamagotchi/src/main/java/com/maimai/tamagotchi/menu/implementation/ShopMenu.java
@@ -1,27 +1,86 @@
 package com.maimai.tamagotchi.menu.implementation;
 
-import com.maimai.tamagotchi.mascot.subject.ConsumableSubject;
+import com.maimai.tamagotchi.manager.MoneyManager;
 import com.maimai.tamagotchi.menu.SimpleMenu;
-import com.maimai.tamagotchi.menu.data.ShopMenuData;
 import com.maimai.tamagotchi.util.console.Console;
+import lombok.SneakyThrows;
 import me.yushust.inject.InjectAll;
+import me.yushust.inject.InjectIgnore;
 
+import java.util.Scanner;
 import java.util.function.Consumer;
 
 @InjectAll
 public class ShopMenu extends SimpleMenu {
 
-    private ShopMenuData shopMenuData;
+    @InjectIgnore
+    private final Scanner scanner = new Scanner(System.in);
 
-    private void usageCurrency(ConsumableSubject subject) {
-        shopMenuData.getCurrency();
-    }
+    private SelectionMenu selectionMenu;
+    private MoneyManager moneyManager;
 
+    @SneakyThrows
     @Override
     public void displayMenu() {
-        shopMenuData.setCurrency(100);
         Console.clearConsole();
-        Console.print("&cActual balance: " + shopMenuData.getCurrency());
+
+        moneyManager.setMoneyCache(100);
+
+        if (moneyManager.getMoneyCache() <= 5) {
+            Console.print("",
+                    "&c&l¡ALERTA! &1» &r&dTú dinero es &b" + moneyManager.getMoneyCache() + "&d!",
+                    "",
+                    "&b&lSeguridad &1» &r&cHey! alto ahí &d&l" + CreatePetMenu.getMascotEntity().getOwner() +
+                            "&r&c, lo lamento pero no puedo dejarte pasar.",
+                    "Debido a que no tienes dinero suficiente para comprar algo en nuestra tienda",
+                    "consigue más y regresa en un rato... &r&d(>'-'<)&r",
+                    ""
+            );
+
+            returnSelectionMenu();
+            return;
+        }
+
+        Console.print("&cActual balance: " + moneyManager.getMoneyCache() + "!&r");
+
+        System.out.println("Debug replace money");
+        moneyManager.replaceMoneyCache(91);
+        moneyManager.replaceMoneyCache(100);
+
+        System.out.println("Add money debug");
+        moneyManager.addMoneyCache(100);
+        moneyManager.addMoneyCache(23);
+        moneyManager.addMoneyCache(4);
+
+        System.out.println("Remove money debug");
+        moneyManager.removeMoneyCache(99);
+        moneyManager.removeMoneyCache(27);
+
+        returnSelectionMenu();
+    }
+
+    private void returnSelectionMenu() {
+        Console.print(
+                "",
+                "&d&l[&b&l0&d&l] &rRegresa al menu principal. &c🎯&r",
+                "");
+
+
+        byte option = Byte.parseByte(scanner.nextLine());
+
+        switch (option) {
+            case 0:
+                selectionMenu.displayMenu();
+                break;
+            case 1:
+                System.out.println("Alto ahí viejo, esta opción no esta disponible por el momento.");
+                displayMenu();
+                break;
+            default:
+                System.out.println("Select an valid option");
+                displayMenu();
+                break;
+        }
     }
 
     @Override
