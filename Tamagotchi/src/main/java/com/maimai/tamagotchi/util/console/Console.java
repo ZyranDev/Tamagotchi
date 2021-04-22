d31cd51e6d24a1279a9d3afec335e4b42bc3eab0
vCamilx <72714741+vCamilx@users.noreply.github.com>
2021-04-22 18:28:21 -0500

    Added documentation

--- Tamagotchi/src/main/java/com/maimai/tamagotchi/util/console/Console.java
+++ Tamagotchi/src/main/java/com/maimai/tamagotchi/util/console/Console.java
@@ -1,19 +1,37 @@
 package com.maimai.tamagotchi.util.console;
 
+import com.sun.istack.internal.NotNull;
+
 import java.util.Scanner;
 
 public class Console {
 
     public Scanner scanner = new Scanner(System.in);
 
+    /**
+     * <p>
+     * Console.print translate color codes & print array as list
+     * </p>
+     *
+     * @param text - Accept the input of an array as list (String)
+     * @return print strings from text & translate it color codes
+     * @see ChatColor
+     */
+
+    @NotNull
     public static void print(String... text) {
         for (String str : text) {
             System.out.println(ChatColor.translateColorCodes(str));
         }
     }
 
+    /**
+     * @return 80 reset lines from ChatColor
+     * @see ChatColor
+     */
+
     public static void clearConsole() {
-        for (byte i = 0; i < 40; i++) {
+        for (byte i = 0; i < 80; i++) {
             System.out.println(ChatColor.translateColorCodes("&r"));
             i++;
         }
